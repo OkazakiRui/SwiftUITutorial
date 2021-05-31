92e277299550670a635f716f33001dfba08b3696
RuiOkazaki <okazaki200226@gmail.com>
2021-05-31 09:22:54 +0900

    first commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 岡崎流依 on 2021/05/31.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
