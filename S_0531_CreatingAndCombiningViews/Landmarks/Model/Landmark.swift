c02852da8907c5a278a1b4b5144284342bd7efe6
RuiOkazaki <okazaki200226@gmail.com>
2021-06-02 03:08:08 +0900

    section2

--- S_0531_CreatingAndCombiningViews/Landmarks/Model/Landmark.swift
+++ S_0531_CreatingAndCombiningViews/Landmarks/Model/Landmark.swift
@@ -10,27 +10,27 @@ import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark:Codable, Hashable {
-    var id:Int
-    var name:String
-    var park:String
-    var state:String
-    var description:String
-    
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-    
+
     private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
+    }
+
     struct Coordinates: Hashable, Codable {
-        var latitude:Double
+        var latitude: Double
         var longitude: Double
     }
-    
-    private var coordinate:Coordinates
-    var locationCoordinate:CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
-    }
-    
 }
