742dc541ef9ddbf1f8a6c07da93653a23b9340c3
RuiOkazaki <okazaki200226@gmail.com>
2021-06-04 21:34:24 +0900

    section end

---
+++ S_0604_DrawingPathsAndShapes/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,21 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by 岡崎流依 on 2021/06/04.
+//  Copyright © 2021 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
